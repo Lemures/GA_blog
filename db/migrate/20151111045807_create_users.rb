bd25665c1f93b8be75e24ad142521c5575d64782
enderki <msmith3dart@icloud.com>
2015-11-11 05:04:36 +0000

    Working Devise

---
+++ db/migrate/20151111045807_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :firstName
+      t.string :lastName
+      t.string :username
+      t.string :aboutMe
+
+      t.timestamps null: false
+    end
+  end
+end
