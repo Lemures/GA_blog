ea6433c13972a2d85a13a66b2e13beb3a073023f
rubymine <msmith3dart@icloud.com>
2015-11-17 20:30:22 -0800

    changed @user in home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,13 +2,14 @@ class HomeController < ApplicationController
   #before_action :authenticate_user!
     def index
       if user_signed_in?
-        @user = User.find(params[:id])
+        #@user = User.find(params[:id])
+        @user = current_user
 
         @posts = User.find(@user.id).posts
 
         @userFullName = [@user.firstName, @user.lastName].join(' ')
 
-        redirect_to users_path
+        #redirect_to users_path
       else
 
        redirect_to new_user_session_path
